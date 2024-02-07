f53b13499f0db5257de8160882e60f71b8ebfb88
deepkanwarsingh <108052609+deepkanwarsingh@users.noreply.github.com>
2024-02-07 19:07:03 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Addtodo from './components/Addtodo'
+import Todo from './components/Todo'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <>
+  <h1 >Redux tool Kit</h1>
+    <Addtodo/>
+    <Todo/>
+
+  </>
+  )
+}
+
+export default App
